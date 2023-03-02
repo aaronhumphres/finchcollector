27d82c693fd25609583358b7a3f9dc10d7fb6603
Aaron Humphres <aaronhumphres@yahoo.com>
2023-03-02 13:55:31 -0700

    added finches index

---
+++ README.md
@@ -0,0 +1,12 @@
+# Finch Collector
+
+## User Stories
+- As a User, when I click the View All My Finches link, I want to see a page listing all of my finches.
+
+
+
+## Routes
+
+route controller action 
+/finches  finches  index  view all finches
+/finches/:id show finches view one finch
--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,9 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+#index route for finches
+def finches_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'finches/index.html', {'finches': finches })
+
+
