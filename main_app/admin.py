16b7e50a1e6f49c3e002a8f28b7209de3c6b9908
Aaron Humphres <aaronhumphres@yahoo.com>
2023-03-06 08:04:09 -0700

    completed deliverable

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 # import your models here
-from .models import Finch
+from .models import Finch, Feeding, Toy
 
-# Register your models here
+# Register your models here.
 admin.site.register(Finch)
+# register our new feeding model
+admin.site.register(Feeding)
+
+admin.site.register(Toy)
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,10 +2,27 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
-  path('about/', views.about, name='about'),
-    # route for finches index
-  path('finches/', views.finches_index, name='index'),
-  path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
+    # using an empty string here makes this our root route
+    # views.home refers to a view that renders a file
+    # the name='home' kwarg gives the route a name
+    # naming routes is optional, but best practices
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    # paths for finches
+    path('finches/', views.finches_index, name='index'),
+    path('finchess/create/', views.FinchCreate.as_view(), name='finchess_create'),
+    path('finchess/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
+    path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finches_delete'),
+    path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+    path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
+    # add association
+    # add unassociation
+    # index, show, create, update, delete
+    path('toys/', views.ToyList.as_view(), name='toys_index'),
+    path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
 ]
+
 
